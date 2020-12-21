14d9741aad73ca90907e739d929788d85bef54d4
packmad <simone.aonzo@gmail.com>
2020-12-22 00:30:33 +0100

    get_nongibberish_string

--- gibberish_score/gibberish_score.py
+++ gibberish_score/gibberish_score.py
@@ -58,6 +58,20 @@ class ProbabilityMarkovChain:
         else:
             raise Exception('Logic flawed')
 
+    def get_nongibberish_string(self, length: int) -> str:
+        if length < 2:
+            raise Exception('Length must be greater than 1')
+        model: DiGraph = self.build_model()
+        out_str = []
+        char = secrets.choice(string.ascii_lowercase)
+        out_str.append(char)
+        while len(out_str) < length:
+            n_w = [(n, model[char][n]['weight']) for n in model[char]]
+            n_w.sort(key=lambda tup: tup[1], reverse=True)
+            char = secrets.choice(n_w[:3])[0]
+            out_str.append(char)
+        return ''.join(out_str)
+
     def get_probability_walk(self, s: str) -> List[float]:
         s = s.lower()
         model: DiGraph = self.build_model()
@@ -73,7 +87,7 @@ class ProbabilityMarkovChain:
 
     def get_score(self, s: str) -> float:
         len_s = len(s)
-        if 0 >= len_s >= 1:
+        if len_s < 2:
             raise Exception('String length must be greater than 1')
         log_sum = sum([math.log(n) for n in self.get_probability_walk(s) if n > 0.0])
         if log_sum == 0.0:
@@ -91,32 +105,32 @@ class GibberishScore:
     def get_gibberish_score(self, input_string: str) -> float:
         return self.rmc.get_score(input_string)
 
+    def get_nongibberish_string(self, length: int):
+        return self.rmc.get_nongibberish_string(length)
 
-def gibberish_score_factory() -> GibberishScore:
+
+def gibberish_score_factory(dataset_name: str = 'english') -> GibberishScore:
     datasets_folder = join(pathlib.Path(__file__).parent.parent, 'datasets')
     assert isdir(datasets_folder)
-    english_words_txt = join(datasets_folder, 'english_words.txt')
-    assert isfile(english_words_txt)
+    words_txt = join(datasets_folder, f'{dataset_name}_words.txt')
+    assert isfile(words_txt)
     models_folder = join(pathlib.Path(__file__).parent.parent, 'models')
     assert isdir(models_folder)
-    eng_words_pickle = join(models_folder, 'english_words.pickle')
-    if not isfile(eng_words_pickle):
+    words_pickle = join(models_folder, f'{dataset_name}_words.pickle')
+    if not isfile(words_pickle):
         rmc = ProbabilityMarkovChain()
-        rmc.training(english_words_txt)
-        rmc.save_model(eng_words_pickle)
-    return GibberishScore(eng_words_pickle)
+        rmc.training(words_txt)
+        rmc.save_model(words_pickle)
+    return GibberishScore(words_pickle)
 
 
 def generate_random_string(l: int) -> str:
-    secrets_gen = secrets.SystemRandom()
-    alphabet = string.ascii_letters
-    rnd_str = ''.join(secrets.choice(alphabet) for i in range(l))
-    return rnd_str
+    return ''.join(secrets.choice(string.ascii_lowercase) for i in range(l))
 
 
 if __name__ == '__main__':
     #TODO create tests
-    gs: GibberishScore = gibberish_score_factory()
+    gs: GibberishScore = gibberish_score_factory('english')
 
     test_strings = [
         'lamer',
@@ -125,6 +139,8 @@ if __name__ == '__main__':
         'pro',
         'antani',
     ]
+    for ii in range(2,8):
+        test_strings.append(gs.get_nongibberish_string(ii))
 
     test_strings.extend([generate_random_string(len(s)) for s in test_strings])
     for s in test_strings:
